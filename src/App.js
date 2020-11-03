8498c98bf4979a67bc0fa319899f9661d9b1eaa9
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-11-03 14:59:22 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, {useEffect, useState} from 'react';
-
-
 import './App.css';
 import Movie from './components/Movie';
-const FEATURED_API = https://api.themoviedb.org/3/discover/movie?api_key=3c1a0e44efdac85f32baaab406391ea4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1
 
-const IMG_API = https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=3c1a0e44efdac85f32baaab406391ea4&language=en-US
-const SEARCH_API = https://api.themoviedb.org/3/search/movie?api_key=3c1a0e44efdac85f32baaab406391ea4&language=en-US&page=1&include_adult=false
+const FEATURED_API ="https://api.themoviedb.org/3/discover/movie?api_key=3c1a0e44efdac85f32baaab406391ea4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
 
+//const IMG_API ="https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=3c1a0e44efdac85f32baaab406391ea4&language=en-US";
+//const SEARCH_API ="https://api.themoviedb.org/3/search/movie?api_key=3c1a0e44efdac85f32baaab406391ea4&language=en-US&page=1&include_adult=false";
 
 
 function App() {
  
   const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+
+    fetch(FEATURED_API)
+    .then(res => res.json())
+    .then(data =>{
+      console.log(data)
+      setMovies(data)
+    })
+  }, [])
+  
   return (
-    <div className="App">
-      {movies.map(map => (
+    <div>
+      {movies.length > 0 && movies.map((movie) => (
               <Movie/>
       ))}
     </div>
